29ee6b237c584e76c66159172bb8fee7f442d12f
MatTerra <mateus.b.s.terra@gmail.com>
2021-05-12 18:11:41 -0300

    Detecção de erros via hamming

--- src/CamadaEnlace/CorrecaoDeErros.cpp
+++ src/CamadaEnlace/CorrecaoDeErros.cpp
@@ -4,6 +4,8 @@
 
 #include "CorrecaoDeErros.hpp"
 
+bool checkHammingParity(const std::bitset<8> &byte_);
+
 std::string CamadaDeEnlaceTransmissoraControleDeErroHamming(std::string quadros) {
     bitStream quadrosEmBits = toBinary(quadros);
     bitStream quadrosCodificados;
@@ -52,21 +54,40 @@ bool makeSum(std::bitset<8> n) {
 }
 
 std::string CamadaDeEnlaceReceptoraControleDeErroHamming(std::string quadros) {
-    bitStream quadrosEmBits = toBinary(quadros);
-    bitStream quadrosDecodificados;
-    std::bitset<8> matrizGeradora[] = {0b10101010, 0b01100110, 0b00011110, 0b11111111};
+    bitStream binaryFrames = toBinary(quadros);
+    bitStream decodedFrames;
+    std::bitset<8> messageMatrix[] = {0b00100000, 0b00001000, 0b00000100, 0b00000010};
+
+
+    for (int i=0; i < binaryFrames.size(); i += 2) {
+        std::bitset<8> byte_;
+        std::bitset<8> messageByte;
+
+        byte_ = binaryFrames[i];
+        if (checkHammingParity(byte_) != 0)
+            std::cout << "Erro detectado!" << std::endl;
+        for(size_t i = 0; i < 4; i++)
+            messageByte[7 - i] = makeSum(messageMatrix[i] & byte_);
+
+        byte_ = binaryFrames[i + 1];
+        if (checkHammingParity(byte_) != 0)
+            std::cout << "Erro detectado!" << std::endl;
+        for (size_t i = 0; i < 4; i++)
+            messageByte[3 - i] = makeSum(messageMatrix[i] & byte_);
+
+
+        decodedFrames.insert(decodedFrames.end(), messageByte);
+    }
+
+    return fromBinary(decodedFrames);
+}
+
+bool checkHammingParity(const std::bitset<8> &byte_) {
+    std::bitset<8> parityMatrix[] = {0b10101010, 0b01100110, 0b00011110, 0b11111111};
     std::bitset<4> verificationNyble;
 
-    for (auto byte_ : quadrosEmBits) {
-        for(size_t i = 0; i < 4; i++)
-            verificationNyble[3 - i] = makeSum(matrizGeradora[i] & byte_);
+    for (size_t i = 0; i < 4; i++)
+        verificationNyble[3 - i] = makeSum(parityMatrix[i] & byte_);
 
-        if(verificationNyble != 0) {
-            std::cout << "Erro detectado!" << std::endl;
-        }
-//        quadrosDecodificados.insert(quadrosDecodificados.end(), verificationNyble);
-    }
-//    fromBinary(quadrosDecodificados);
-
-    return quadros;
+    return verificationNyble == 0;
 }
